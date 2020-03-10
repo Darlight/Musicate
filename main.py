7e8fc506738413693a9ee4f15bbe87559b92bce1
Darlight <andres.perdomo09@gmail.com>
2020-03-09 21:23:51 -0600

    Aprendiendo como conectar python con postgres
    Debemos aplicar Kivy para la UI.

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Universidad del Valle 
+Sección 20
+Grupo - 08
+Josue Sagastume 18173
+Diego Solorzano 18 
+Mario Perdomo 18029
+
+"""
+
+# En el main, nos crearemos los queries como agregar, eliminar y  entre otras opciones
