9cc4f622ebb9a8fb6bed3e29758e79d5fbac38d2
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2020-03-25 17:43:40 -0600

    queries

--- db.py
+++ db.py
@@ -11,13 +11,13 @@ con = psycopg2.connect(
 
 #create a cursor
 cur = con.cursor()
-menu = "\n\t1. Artistas con más álbumes publicados\n\t2. Generos con más canciones\n\t3. Duración de cada playlist\n\t4. Canciones de mayor duracion con la informacion de sus artistas\n\t5. Artistas que han registrado mas canciones\n\t6. Promedio de duracion de canciones por genero\n\t7. Cantidad de artistas diferentes por playlist\n\t8. Artistas con mas diversidad de generos musicales\n\t9. Buscar cancion\n\t10. Buscar artista\n\t11. Agregar usuario\n\t12. Buscar usuario\n\t13. Agregar cancion\n\t14. Agregar album\n\t15. Agregar artista\n\t16. Login con usuario existente\n\t17. Salir"
+menu = "\n\t1. Artistas con más álbumes publicados\n\t2. Generos con más canciones\n\t3. Duración de cada playlist\n\t4. Canciones de mayor duracion con la informacion de sus artistas\n\t5. Artistas que han registrado mas canciones\n\t6. Promedio de duracion de canciones por genero\n\t7. Cantidad de artistas diferentes por playlist\n\t8. Artistas con mas diversidad de generos musicales\n\t9. Buscar cancion\n\t10. Buscar artista\n\t11. Agregar usuario\n\t12. Buscar usuario\n\t13. Agregar cancion\n\t14. Agregar album\n\t15. Agregar artista\n\t16. Login con usuario existente\n\t17. Modificar cancion\n\t18. Modificar album\n\t19.Modificar artista\n\t20. Salir"
 
 print("BIENVENIDO A MUSICATE")
 print("Ingrese la opcion que desea buscar:")
 print(menu)
 opcion = int(input())
-while (opcion != 17):
+while (opcion != 20):
     #Artistas con mas albumes publicados
     if (opcion == 1):
         cur.execute("SELECT artist.name, COUNT(album.albumid) FROM artist INNER JOIN album ON artist.artistid = album.artistid GROUP BY artist.name ORDER BY COUNT(album.albumid) DESC LIMIT 5")
@@ -101,7 +101,7 @@ while (opcion != 17):
     #Buscar cancion
     elif (opcion == 9):
         cancion = input("Ingrese el nombre de la cancion: ")
-        cur.execute("SELECT t.name, t.composer FROM track t WHERE t.name = %s", (cancion,))
+        cur.execute("SELECT t.name, t.composer FROM track t WHERE t.name ILIKE %s LIMIT 10", (cancion,))
         opcion1 = cur.fetchall()
         for r in opcion1:
             print(f"{r[0]}  {r[1]}")
@@ -112,7 +112,7 @@ while (opcion != 17):
     #Buscar artista
     elif (opcion == 10):
         artista = input("Ingrese el nombre del artista: ")
-        cur.execute("SELECT a.artistid, a.name FROM artist a WHERE a.name = %s", (artista,))
+        cur.execute("SELECT a.artistid, a.name FROM artist a WHERE a.name ILIKE %s LIMIT 10", (artista,))
         opcion1 = cur.fetchall()
         for r in opcion1:
             print(f"{r[0]}  {r[1]}")
@@ -254,6 +254,84 @@ while (opcion != 17):
         
         print(menu)
         opcion = int(input())
+
+    #Modificar la informacion de una cancion
+    if (opcion == 17):
+        name1 = input("Ingrese el nombre de la cancion que desea modificar: ")
+        cur.execute("SELECT track.trackid FROM track WHERE track.name = %s ", (name1,))
+        opcion1 = cur.fetchall()
+        trackid = 0
+        for r in opcion1:
+            trackid = r[0]
+
+        newname = input("Ingrese el nuevo nombre de la cancion: ")
+
+        album = input("Ingrese el album de la cancion: ")
+        cur.execute("SELECT album.albumid FROM album WHERE album.title = %s ", (album,))
+        opcion1 = cur.fetchall()
+        albumid = 0
+        for r in opcion1:
+            albumid = r[0]
+
+        mediatype = input("Ingrese el mediatype de la cancion: ")
+        cur.execute("SELECT mediatype.mediatypeid FROM mediatype WHERE mediatype.name = %s ", (mediatype,))
+        opcion1 = cur.fetchall()
+        mediatypeid = 0
+        for r in opcion1:
+            mediatypeid = r[0]
+
+        genre = input("Ingrese el genero de la cancion: ")
+        cur.execute("SELECT genre.genreid FROM genre WHERE genre.name = %s ", (genre,))
+        opcion1 = cur.fetchall()
+        genreid = 0
+        for r in opcion1:
+            genreid = r[0]
+
+        composer = input("Ingrese el artista de la cancion: ")
+        milliseconds = input("Ingrese la duracion de la cancion: ")
+        size = input("Ingrese el tamano de la cancion en bytes: ")
+        unitprice = input("Ingrese el precio de la cancion: ")
+
+        cur.execute("UPDATE Track SET name = %s, albumid=%s, mediatypeid=%s, genreid=%s, composer=%s, milliseconds=%s, size=%s, unitprice=%s WHERE trackid=%s", (newname, albumid, mediatypeid, genreid, composer, milliseconds, size, unitprice, trackid,))
+        con.commit()
+        print("Se ha actualizado la cancion en la base de datos")
+
+    #Modificar album
+    elif (opcion == 18):
+        title = input("Ingrese el nombre del album que desea modificar: ")
+        cur.execute("SELECT album.albumid FROM album WHERE album.title = %s ", (title,))
+        opcion1 = cur.fetchall()
+        albumid = 0
+        for r in opcion1:
+            albumid = r[0]
+
+        newtitle = input("Ingrese el nuevo nombre del album: ")
+
+        artist = input("Ingrese el artista del album: ")
+        cur.execute("SELECT artist.artistid FROM artist WHERE artist.name = %s ", (artist,))
+        opcion1 = cur.fetchall()
+        artistid = 0
+        for r in opcion1:
+            artistid = r[0]
+
+        cur.execute("UPDATE album SET title = %s, artistid=%s WHERE albumid=%s", (newtitle, artistid, albumid,))
+        con.commit()
+        print("Se ha actualizado el album en la base de datos")
+
+    #Modificar artista
+    elif (opcion == 19):
+        name = input("Ingrese el nombre del artista que desea modificar: ")
+        cur.execute("SELECT artist.artistid FROM artist WHERE artist.name = %s ", (name,))
+        opcion1 = cur.fetchall()
+        artistid = 0
+        for r in opcion1:
+            artistid = r[0]
+
+        newname = input("Ingrese el nuevo nombre del artista: ")
+
+        cur.execute("UPDATE Artist SET name=%s, WHERE artistid=%s", (newname, artistid))
+        con.commit()
+        print("Se ha actualizado el artista en la base de datos")
     else:
         print(menu)
         opcion = int(input())
